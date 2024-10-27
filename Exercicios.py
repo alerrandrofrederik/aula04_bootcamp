d010d6718a9383e567d9038ef2844de46d6d68fb
Alerrandro <alerrandrofrederik@gmail.com>
2024-10-27 15:57:07 -0300

    resolução item 03

--- Exercicios.py
+++ Exercicios.py
@@ -9,13 +9,24 @@
 
 # 2 - Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
 
-lista: list = ["Python", "Java", "C++", "JavaScript"]
+# lista: list = ["Python", "Java", "C++", "JavaScript"]
 
-lista.remove('C++')
-lista.append('Ruby')
+# lista.remove('C++')
+# lista.append('Ruby')
 
-print(lista)
+# print(lista)
 
 # 3 - Crie um dicionário para armazenar informações de um livro, incluindo título, autor e ano de publicação. Imprima cada informação.
+
+livro: dict = {
+'título': 'Harry Potter',
+"autor": 'J.K. Rowling',
+"ano": 1997
+}
+
+for chave, valor in livro.items():
+    print(f'{chave}: {valor}')
+
+
 # 4 - Escreva um programa que conta o número de ocorrências de cada caractere em uma string usando um dicionário.
 # 5 - Dada a lista ["maçã", "banana", "cereja"] e o dicionário {"maçã": 0.45, "banana": 0.30, "cereja": 0.65}, calcule o preço total da lista de compras.
